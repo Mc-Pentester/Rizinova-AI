94251b594df0931ad5fc8a791a18095bc8a82cdd
Mc-Pentester <mcintoshfr@gmail.com>
2026-06-02 20:26:53 -0400

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    PLAN_CHOICES = [('free', 'Gratuit'), ('pro', 'Professionnel')]
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    plan = models.CharField(max_length=20, choices=PLAN_CHOICES, default="free")
+    daily_requests = models.IntegerField(default=0)
+    last_request_date = models.DateField(default=timezone.now)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
